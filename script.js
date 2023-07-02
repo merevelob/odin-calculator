46da5c010fdbf5bd0619489ff2b13372c40624d6
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-07-02 14:17:47 -0500

    Make calculator work

--- script.js
+++ script.js
@@ -32,3 +32,56 @@ function populateDisplay(event) {
 buttons.forEach(button => {
     button.addEventListener('click', populateDisplay);
 });
+
+// 6. Make the calculator work
+
+const oneValueRegex = /^\d+[-+*/]$/;
+const moreValuesRegex = /^\d+[-+*/]\d+/;
+
+function storeValues(event) {
+    if (event.type === 'click' || Object.keys(operations).includes(event.key)) {
+        if (oneValueRegex.test(display.value)) {
+            num1 = +(display.value).slice(0, -1);
+            console.log(num1);
+            operator = display.value.slice(-1);
+            console.log(operator);
+        } else if (moreValuesRegex.test(display.value)) {
+            storeNum2();
+            const secondOperator = display.value.slice(-1);
+            const newNum1 = operate(operator, num1, num2);
+            num1 = newNum1;
+            operator = secondOperator;
+            display.value = newNum1 + secondOperator;
+            display.focus();
+        }
+    }  
+}
+
+// Save num1 and operator
+const operators = document.querySelectorAll('.operators > .toDisplay');
+
+operators.forEach(operator => {
+    operator.addEventListener('click', storeValues);
+});
+
+display.addEventListener('keyup', storeValues);
+
+// Save num2, and display solution
+
+function storeNum2() {
+    num2 = +display.value.match(/(?<=\d+[-+*/])\d+(?=[-+*/]?)/);
+    console.log(num2);
+}
+
+function displaySolution(event) {
+    if (event.type === 'click' || event.key === 'Enter') {
+        storeNum2();
+        display.value = operate(operator, num1, num2);
+        display.focus();
+    }
+}
+
+const equals = document.querySelector('.equals');
+
+equals.addEventListener('click', displaySolution);
+display.addEventListener('keydown', displaySolution);
